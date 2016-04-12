427b1a08413ed69b6afba6005836a6cdd2f3ff48
Yair Hernandez <yair.hernandez@evol-tech.com>
2016-04-12 14:34:49 -0500

    Se terminaron reportes de venta de todo el periodo, uso de optional parameters en routes

--- app/controllers/Venta.scala
+++ app/controllers/Venta.scala
@@ -12,6 +12,7 @@ import play.api.mvc._
 import play.twirl.api.TemplateMagic.anyToDefault
 
 import scala.collection.immutable.ListMap
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by yair on 11/04/16.
@@ -23,11 +24,19 @@ import scala.collection.immutable.ListMap
    }
 
   def ventasPeriodo = Action {
-    val data = getData()
+    val data = getVentasPeriodo()
     Ok(views.html.ventas.ventasPeriodo(data))
   }
 
-  def getData(): Map[String, Int] = {
+  def ventasPeriodoFamilia = Action { request =>
+    val params = request.queryString.map { case (k,v) => k -> v.mkString }
+    val familia = params.get("familia").get
+    val data = getVentasPeriodoFamilia(familia)
+    val matriz = getMatrixData(familia)
+    Ok(views.html.ventas.ventasPeriodoFamilia(familia, data, matriz))
+  }
+
+  def getVentasPeriodo(): Map[String, Int] = {
     var data: Map[String, Int] = Map()
     db.withConnection{ connection =>
       val statement = connection.createStatement()
@@ -57,12 +66,55 @@ import scala.collection.immutable.ListMap
     data
   }
 
-  def createChart() = Action {
+  def getVentasPeriodoFamilia(familia: String): Map[String, Int] = {
+    var data: Map[String, Int] = Map()
+    db.withConnection{ connection =>
+      val statement = connection.createStatement()
+      statement.execute("DROP TABLE IF EXISTS venta_familias;")
+      statement.execute( s"""SELECT product.product_id, product.primary_product_category_id, coalesce( sum(invoice_item.quantity),0) AS cantidad
+        INTO TEMP venta_familias
+          FROM product, invoice_item, invoice
+          WHERE 1 = 1
+            AND invoice.invoice_id = invoice_item.invoice_id
+            AND product.product_id = invoice_item.product_id
+            AND invoice.invoice_type_id = 'SALES_INVOICE'
+            AND invoice.invoice_fis <> 'HISTORICA'
+            AND invoice.status_id in ( 'INVOICE_READY', 'INVOICE_PAID', 'INVOICE_IN_PROCESS')
+            AND product.primary_product_category_id = '${familia}'
+          GROUP BY 1, 2
+          ORDER BY 1,2;""")
+
+      val resultSet = statement.executeQuery( s"""SELECT product.product_id, coalesce( venta_familias.cantidad, 0) as venta
+        FROM product left outer join venta_familias on product.product_id = venta_familias.product_id
+        WHERE  1=1
+          AND product.primary_product_category_id = '${familia}'
+        ORDER BY 2 DESC;""");
+
+      while (resultSet.next()) {
+        val categoryId = resultSet.getString(1)
+        val venta = resultSet.getInt(2)
+        data += categoryId -> venta
+      }
+    }
+    println(data.mkString)
+    data = ListMap(data.toSeq.sortWith(_._2 >_._2):_*)
+    data
+  }
+
+  def selectData(indicator: Option[String]) = {
+    indicator match {
+      case Some(s) => getVentasPeriodoFamilia(s)
+      case None => getVentasPeriodo()
+    }
+  }
+
+  def createChart(familia: Option[String], title: String, titleX: String, titleY: String) = Action { request =>
+    println("FAMILIA: " + familia)
+    val data = selectData(familia)
+    println("DATA: " + data.mkString)
     val MimeType = "image/png"
     try {
-      val data = getData()
-      val imageData = generateBarChart(data)
-      //Redirect(routes.Venta.showVentaFamilias(imageData))
+      val imageData = generateBarChart(data, title, titleX, titleY)
       Ok(imageData).as(MimeType)
     } catch {
       case e: Exception =>
@@ -70,7 +122,7 @@ import scala.collection.immutable.ListMap
     }
   }
 
-  def generateBarChart(data: Map[String, Int]): Array[Byte] = {
+  def generateBarChart(data: Map[String, Int], title: String, titleX: String, titleY: String): Array[Byte] = {
     val values = new DefaultCategoryDataset()
 
     var width = 0
@@ -83,14 +135,18 @@ import scala.collection.immutable.ListMap
         val venta = v
         values.addValue(venta, "", productId)
         width += 50
-        top = top + 1
+        top += 1
       }
     }
 
+    if (data.size < 5) {
+      width = 300
+    }
+
     val chart = ChartFactory.createBarChart(
-      s"Top 20 Venta Familia familia Enero 2016",
-      "Productos",
-      "Venta",
+      title,
+      titleX,
+      titleY,
       values,
       PlotOrientation.VERTICAL,
       false, false, false
@@ -105,5 +161,83 @@ import scala.collection.immutable.ListMap
     ChartUtilities.writeBufferedImageAsPNG(byteArray, image);
 
     return byteArray.toByteArray()
+  }
+
+  def getMatrixData(familia: String): (Array[Array[String]]) = {
+    val ejeX = new ArrayBuffer[String]()
+    val ejeY = new ArrayBuffer[String]()
+    var prods: Map[Int, String]= Map()
+
+    db.withConnection { connection =>
+      val statement = connection.createStatement
+      val resultSet = statement.executeQuery(s"SELECT * FROM i_l_t_atributos_matrix WHERE product_category_id = '${familia}'")
+
+      while(resultSet.next) {
+        println(resultSet.getString(1))
+        if (resultSet.getString(2) == "X") {
+          ejeX.insert(resultSet.getInt(4), resultSet.getString(3))
+        } else if (resultSet.getString(2) == "Y") {
+          ejeY.insert(resultSet.getInt(4), resultSet.getString(3))
+        }
+      }
+
+    }
+
+    db.withConnection { connection =>
+      val statement = connection.createStatement
+      val prod = statement.executeQuery(s"""SELECT product.product_id, product.posicion_matriz AS pos,
+        to_char(coalesce( venta_familias.cantidad, 0), '999G999D') AS venta
+        FROM product LEFT OUTER JOIN venta_familias ON product.product_id = venta_familias.product_id
+        WHERE product.primary_product_category_id = '${familia}'
+        ORDER BY 2""")
+      while(prod.next()){
+        val pos = prod.getInt("pos")
+        val venta = prod.getString("venta")
+        prods +=  pos -> venta
+      }
+    }
+
+    prods = ListMap(prods.toSeq.sortBy(_._1):_*)
+
+    val numRenglones = ejeY.length
+    val numColumnas =   ejeX.length
+
+    var mat = Array.ofDim[String](numRenglones + 1, numColumnas + 1)
+
+    var x = 1
+    for(value <- ejeX){
+      mat(0)(x) = value
+      x += 1
+    }
+
+    var y = 1
+    for (valueY <- ejeY) {
+      mat(y)(0) = valueY
+      y += 1
+    }
+
+    var renglon = 1
+    var newIdx = 1
+
+    prods.foreach { case (k, v) =>
+      if (renglon <= numRenglones) {
+        if (k <= numColumnas) {
+          mat(1)(k) = v
+        } else {
+          if (newIdx <= numColumnas) {
+            mat(renglon)(newIdx) = v
+          } else {
+            newIdx = 1
+            renglon += 1
+            mat(renglon)(newIdx) = v
+          }
+        }
+        newIdx += 1
+      }
+    }
+
+    print(mat.map(_.mkString).mkString(" \n "))
+
+    mat
   }
 }
